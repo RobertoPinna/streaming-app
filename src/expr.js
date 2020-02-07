98120ed3bf7bf60489a9a574fbdf15229cbcccbb
Roberto Pinna <pinnapinna@hotmail.it>
2020-02-07 16:03:09 +0100

    changes

--- src/expr.js
+++ src/expr.js
@@ -11,14 +11,26 @@ const socketio = require('socket.io')
 const hbs = require('hbs')
 const fetch = require('node-fetch')
 var cors = require('cors')
-const robot = require('robotjs')
+//const robot = require('robotjs')
 const d3 = require('d3-scale')
-const fs = require('fs');
+const fs = require('fs')
+
+//const add_device = require(__dirname+'')
+
+console.log(__dirname+'/../')
 
 const {spawn} = require('child_process')
 
-let rawdata = fs.readFileSync(__dirname+'/../devices_database.json');
-
+let rawdata = fs.readFileSync(__dirname+'/utils/devices_database.json');
+/*
+fs.writeFile(__dirname + "/../../database.json", 'jsonContent', 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+    console.log("JSON file has been saved.");
+})
+*/
 
 var all_devices_tree = {}
 
@@ -29,148 +41,6 @@ try{
     all_devices_tree = JSON.parse(rawdata).all_data
 }catch(e){
     console.log('error reading the file ')
-}
-
-
-
-
-
-// here to create object , i need it to insert it into all_device variable easily
-
-var create_object = ( first_size , second_size , ppi_size , browser , browser_version , os) => {
-    return { value : first_size , next : { value : second_size , next : { value : ppi_size , next : { value : browser , next : { value : browser_version , next : { value : os } } } } } }
-}
-
-
-var add_device = (all , obj , n ) => {
-
-    if(obj != undefined){ // here the condition for exiting from the recursive function (if false) if i go to te end of the object i finished to go throught it
-        if(JSON.stringify(all[obj.value]) == JSON.stringify( {} ) || (all[obj.value] == undefined ) )  { // here i add it , if true
-            all[obj.value] = {}  
-            add_device (all[obj.value] , obj.next , n ) 
-            // here is to see if item added (!= 1 ) or not (==1)
-            return n + 1 
-        }       
-
-        return add_device (all[obj.value] , obj.next , n ) 
-
-    }
-   
-    return n
-
-}
-
-/*
-var remove_device = (the_list , next_value  ) => {
-    
-    if(next_value.next == undefined){
-        if(Object.keys(the_list).includes(next_value.value)){
-            delete the_list[next_value.value]
-            return 1
-        }
-        return 0
-    }
-
-    if(the_list[next_value.value] == undefined)
-        return 0
-    
-    var result = remove_device(the_list[next_value.value] , next_value.next)
-
-    if( result > 0 ){
-        if(result == 2 )
-            return result
-        if(Object.keys(the_list).length  == 1 ){
-            delete the_list[next_value.value]
-            return 1
-        }
-        delete the_list[next_value.value]
-        return 2 
-        
-    }   
-    
-    return 0
-
-}
-*/
-
-var remove_device = ( all_tree , obj ) => {
-
-    if(all_tree[obj.value] == undefined) 
-        return 0 
-
-    if( obj.next  == undefined ){
-        if( JSON.stringify(all_tree[obj.value]) != JSON.stringify({}) )
-        //all_tree == undefined
-            return 0
-        return 1 
-    }
-         
-    var result = remove_device(all_tree[obj.value] , obj.next) 
-
-    if( result == 1 ) {
-        if(Object.keys(all_tree).length > 1) {
-                delete all_tree[obj.value]
-                return 2
-        }
-    }
-    
-    return result 
-
-}
-
-
-
-var print_all_devices_tree = (all_list) => {
-    const{el, all_elements ,  str_list } = print_partial_all_devices_tree(0 , (x) => {
-        if (x == 0)
-            return 'first'
-        if(x == 1 )
-            return 'second'
-        if(x == 2 ) 
-            return 'ppi_size'
-        if(x==3)
-            return 'browser'
-        if(x==4)
-            return 'version'
-        if(x==5)
-            return 'os'
-    } , all_list  , '')
-    return { total_elements : all_elements , total_string : 'I devices a disposizione sono : \n' +str_list } 
-}
-
-
-// use this function to print each element
-
-// check this one 
-var print_partial_all_devices_tree = (x , callback ,  all_list , stringa  ) => {
-  
-    var keys = Object.keys(all_list)
-    var size_list = keys.length
-
-    var total_string = ''
-    var list_elements = [] 
-    var one_el = {} 
-
-
-    for(var i = 0 ; i < size_list ; i++ ) {
-        if(JSON.stringify(all_list[keys[i]]) == JSON.stringify( {} ) ){
-            total_string = keys[i]
-            one_el[callback(x)] = keys[i]
-            return {all_elements : [one_el] , str_list : stringa + keys[i] +  '\n' }
-        }
-        const{all_elements , str_list} =  print_partial_all_devices_tree( x + 1  , callback ,  all_list[keys[i]] , keys[i] + " - " + stringa)
-        all_elements.forEach(el => { 
-            el[callback(x)] = keys[i]
-        })
-        if(JSON.stringify(list_elements) == JSON.stringify([]) )
-            list_elements = all_elements
-        else
-            list_elements.push(all_elements)
-        total_string = total_string +  str_list
-        
-    }
-
-    return {all_elements : list_elements , str_list : total_string}    
 }
 
 
@@ -207,7 +77,7 @@ app.use(cors())
 app.options('*', cors())
 
 app.get('/prova1',  (req,res) => {
-    robot.moveMouse( scale_x(req.query.x_pos) , scale_y(req.query.y_pos) )
+    //robot.moveMouse( scale_x(req.query.x_pos) , scale_y(req.query.y_pos) )
     res.send({speriamo : 'sperem'})
 })
 
@@ -234,7 +104,7 @@ app.get('/install_device' , (req,res) => {
                 return console.log(err);
             }
             console.log("JSON file has been saved.");
-        });
+        })
     }
 
 
@@ -338,7 +208,7 @@ app.get('/coords' , (req,res) => {
 
 app.get('/click' , (req,res) => {
     res.send('scriviamo qualcosa')
-    robot.mouseClick()
+    //robot.mouseClick()
 })
  
 
